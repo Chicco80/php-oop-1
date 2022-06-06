c69f97ff68aeeae4b2181913b01498d387f396ee
Chicco80 <chicco80lisa@gmail.com>
2022-06-06 15:48:35 +0200

    da terminare

--- Classes/Movie.php
+++ Classes/Movie.php
@@ -1,43 +1,43 @@
 <?php
 class Movie{
+    private $img;
     private $title;
     private $genre;
     private $year;
     private $price;
 
 
-    function __construct($_title, $_genre, $_year, $_price){
+    function __construct($_img, $_title, $_genre, $_year, $_price){
+        $this->img = $_img;
         $this->title = $_title;
         $this->genre = $_genre;
         $this->year = $_year;
         $this->price = $_price;
     }
+    public function getImg(){
+        return $this->img;
+    }
+    
+
+
+
     public function getTitle(){
         return $this->title;
     }
-    public function setTitle($_title){
-        $this->title = $_title;
 
-    }
 
     public function getGenres(){
         return $this->genre;
     }
-    public function setGenres($_genres){
-        $this->genre = $_genres;
-    }
+
 
     public function getYear(){
         return $this->year;
     }
-    public function setYear($_year){
-        $this->year = $_year;
-    }
+
 
     public function getPrice(){
         return $this->price;
     }
-    public function setPrice($_price){
-        $this->price = $_price;
-    }
+    
 }
--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 require_once __DIR__ .'/Classes/Movie.php';
 
 
+
 $movies=[
     [
+        'img'=>'./img/leoni.jpg',
         'title'=>'Una Notte da Leoni',
         'genre'=>'Commedia',
         'year'=>'2003',
         'price'=>'3.99'
     ],
     [
+        'img'=>'./img/fight.jpg',
         'title'=>'Fight Club',
         'genre'=>'Dramma',
         'year'=>'1999',
         'price'=>'2.99'
     ],
     [
+        'img'=>'./img/matrix.jpg',
         'title'=>'Matrix',
         'genre'=>'Fantascenza',
         'year'=>'2000',
         'price'=>'1.99'
     ],
     [
+        'img'=>'./img/ring.jpg',
         'title'=>'The Ring',
         'genre'=>'Horror',
         'year'=>'2004',
@@ -39,19 +44,33 @@ $movies=[
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <title>php-oop-1</title>
 </head>
 <body>
     <h1>Lista Film</h1>
     <?php
     foreach($movies as $key=>$value){
-        $movie = new Movie($value['title'], $value['genre'], $value['year'], $value['price'])
+        $movie = new Movie($value['img'], $value['title'], $value['genre'], $value['year'], $value['price'])
     
     ?>
-    <h2>Title: <?php echo $movie->getTitle() ?></h2>
-    <p>Genere:<?php echo $movie->getGenres() ?></p>
-    <p>Anno Uscita:<?php echo $movie->getYear() ?></p>
-    <p>Prezzo:<?php echo $movie->getPrice() ?></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card " >
+                        <img src="<?php echo $movie->getImg() ?>" class="card-img-top" alt="..."> 
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie->getTitle() ?> </h5>
+                                <p class="card-text"> Genere: <?php echo $movie->getGenres() ?></p>
+                                <p> Anno di Uscita: <?php echo $movie->getYear() ?></p>
+                                <p> Prezzo Noleggio: <?php echo $movie->getPrice() ?></p>
+                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
 
     <?php
     }
