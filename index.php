1a79ed90c4c3046835698ef1345507611bfe9989
Chicco80 <chicco80lisa@gmail.com>
2022-06-06 18:15:16 +0200

    ok

--- index.php
+++ index.php
@@ -44,36 +44,46 @@ $movies=[
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-    <title>php-oop-1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <title>oop3</title>
 </head>
 <body>
-    <h1>Lista Film</h1>
-    <?php
-    foreach($movies as $key=>$value){
-        $movie = new Movie($value['img'], $value['title'], $value['genre'], $value['year'], $value['price'])
-    
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-3">
-                <div class="card " >
-                        <img src="<?php echo $movie->getImg() ?>" class="card-img-top" alt="..."> 
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $movie->getTitle() ?> </h5>
-                                <p class="card-text"> Genere: <?php echo $movie->getGenres() ?></p>
+    <h1>Movies</h1>
+
+
+
+    <div class="container d-flex">
+        
+            <?php foreach($movies as $key=>$value){
+                $movie = new Movie($value['img'], $value['title'], $value['genre'], $value['year'], $value['price'])?>
+                <div class="card mx-2 my-4" style="width: 18rem;">
+                    <img src="<?php echo $movie->getImg() ?>" class="card-img-top" alt="..."> 
+                    <div class="card-body">
+                    <h5><?php echo $movie->getTitle() ?> </h5>
+                                <p > Genere: <?php echo $movie->getGenres() ?></p>
                                 <p> Anno di Uscita: <?php echo $movie->getYear() ?></p>
                                 <p> Prezzo Noleggio: <?php echo $movie->getPrice() ?></p>
-                            </div>
                 </div>
-            </div>
-        </div>
     </div>
-    
-    
-
     <?php
-    }
-    ?>
+                    }
+            ?>
 </body>
 </html>
+
+<style>
+    body{
+        background: #cecece;
+    }
+    img{
+        height:500px;
+    }
+
+    .card-body{
+        width:100%;
+        background-color: grey;
+        color:white;
+    }
+
+
+
+</style>
